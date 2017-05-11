dfd5d0ccb919cc103dad953a37164d6e9f7c8dad
Juan Campos <juanico@localhost.localdomain>
2017-05-10 20:41:40 -0600

    Initial commit

---
+++ Ordenador.h
@@ -0,0 +1,31 @@
+#ifndef _CLASE_ORDENADOR
+#define _CLASE_ORDENADOR
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include "time.h"
+using namespace std;
+
+class Ordenador{
+private:
+
+    //Defina los mtodos auxiliares de los algoritmos de ordenamiento solamente ac.
+    //Puede definir cuantos mtodos quiera.
+
+public:
+
+    Ordenador(){};
+    ~Ordenador(){};
+
+    //Si piensa no implementar algunos de los mtodos de ordenamiento, no los borre,
+    //simplemente djelos con el cuerpo vaco para as evitar errores de compilacin, ya
+    //que voy a correr el mismo main para todas las tareas.
+    void seleccion(int * arreglo, int tamano){};
+    void insercion(int * arreglo, int tamano){};
+    void mergesort(int * arreglo, int tamano){}
+    void heapsort(int * arreglo, int tamano){};
+    void quicksort(int * arreglo, int tamano){};
+    void radixsort(int * arreglo, int tamano){};
+    int mediana(int * arreglo, int tamano){};
+};
+#endif
