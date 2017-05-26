a8b55d026ec5bc65cc5614bfe3e82ecd1b9d296d
DanielHerreraPeraza <danieljhp@gmail.com>
2017-05-25 18:32:22 -0600

    Agregada documentacion

--- heapsort.cpp
+++ heapsort.cpp
@@ -3,17 +3,48 @@
 //
 #include "Ordenador.h"
 
+/***
+*Descripcion: Método que crea un montículo.
+*Parámetros:
+*	-int *arreglo: arreglo a ordenar.
+*	-int tamano: tamaño del arreglo.
+*Variables:
+***/
+
 void Ordenador::createHeap(int *arreglo, int tamano) {
     for (int i = tamano / 2 - 1; i >= 0; --i) {
         heapify(i, arreglo, tamano);
     }
 }
 
+/***
+*Descripcion: Método que recibe un arreglo y la posicíon de dos de sus elementos para intercambiarlos.
+*Parámetros:
+*	-int *arreglo: arreglo a ordenar.
+*	-int a: posición del primer elemento del arreglo a intercambiar.
+*	-int b: posición del segundo elemento del arreglo a intercambiar.
+*Variables:
+*   -int temporal: Variable auxiliar utilizada para intercambiar otras variables.
+***/
+
 void Ordenador::swap(int *arreglo, int a, int b) {
     int temporal = arreglo[a];
     arreglo[a] = arreglo[b];
     arreglo[b] = temporal;
 }
+
+/***
+*Descripcion: Método que se encarga de hacer los montículos máximos, es decir, todo nodo hijo es mejor que su nodo padre.
+*Parámetros:
+*	-int *arreglo: arreglo a ordenar.
+*	-int tamano: tamaño del arreglo.
+*   -int nodo: elemento donde se empieza a crear los monticulos.
+*Variables:
+*   -int ultimo_no_hoja: se utiliza para comprobar si un nodo no es una hoja.
+*   -int hijo: asigna el hijo de un nodo si este no es una hoja.
+*   -int hijo_izquierda: representa el hijo izquierdo de un nodo, nodo +1.
+*   -int hijo_derecho: representa el hijo derecho de un nodo, nodo +2.
+***/
 
 void Ordenador::heapify(int nodo, int *arreglo, int tamano) {
     int ultimo_no_hoja = tamano / 2 - 1;
@@ -40,6 +71,14 @@ void Ordenador::heapify(int nodo, int *arreglo, int tamano) {
     }
 }
 
+/***
+*Descripcion: Método auxiliar que imprime el arreglo.
+*Parámetros:
+*	-int *arreglo: arreglo a ordenar.
+*	-int tamano: tamaño del arreglo.
+*Variables:
+***/
+
 void holi(int *arreglo, int tamano) {
     cout << "(" << to_string(arreglo[0]);
     for (int i = 1; i < tamano; ++i) {
@@ -47,6 +86,14 @@ void holi(int *arreglo, int tamano) {
     }
     cout << ")" << endl;
 }
+
+/***
+*Descripcion: Método de ordenamiento mediante el uso de montículos máximos.
+*Parámetros:
+*	-int *arreglo: arreglo a ordenar.
+*	-int tamano: tamaño del arreglo.
+*Variables:
+***/
 
 void Ordenador::heapsort(int *arreglo, int tamano) {
     createHeap(arreglo, tamano);
